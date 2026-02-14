45a8b7643b9b7ca7f343361980724e8eecf5936d
studenote2026 <studenote3@gmail.com>
2026-02-14 15:40:41 +0530

    nodemailer problom solved try

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -2,10 +2,13 @@ import nodemailer from "nodemailer";
 
 export const mailTransporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: Number(process.env.MAIL_PORT),
-  secure: false, // 587 ke liye false
+  port: Number(process.env.MAIL_PORT || 587),
+  secure: false, // true only for 465
   auth: {
     user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
+    pass: process.env.MAIL_PASS, // App Password if Gmail
+  },
+  tls: {
+    rejectUnauthorized: false, // fixes SSL issues on many VPS/GoDaddy
   },
 });
